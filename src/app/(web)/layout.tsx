ecfd2fd3aa4476ef15992f4f81746182e9e5cd7f
Eka Revandi <cimicimicilung4868@gmail.com>
2025-10-24 19:31:13 +0000

    di buat dengan apus aja gasi (_for element p_)

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -140,7 +140,7 @@ export default function WebLayout({ children }: { children: ReactNode }) {
         </div>
         <div className="border-t py-4">
           <p className="text-center text-sm text-muted-foreground">
-            © {new Date().getFullYear()} Learniverse. Dibuat dengan ❤️ oleh Revan.
+            © {new Date().getFullYear()} Learniverse. Revan.
           </p>
         </div>
       </footer>
