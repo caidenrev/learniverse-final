5963268d84024dc9da4adba262926d0d75d6acb9
Eka Revandi <cimicimicilung4868@gmail.com>
2026-02-27 19:36:24 +0700

    Refactor firebase config to use environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9815254143-785c5",
-  "appId": "1:264015769929:web:cec8d18197d1b9240f6a98",
-  "apiKey": "AIzaSyBUINPlAcUVNQHsOCn8LC2I7n2NtFjLmSs",
-  "authDomain": "studio-9815254143-785c5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "264015769929"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
